03535996cf17e75ccf6c7c0da2c7506df238a50d
Davi <davisuperamojesus@gmail.com>
2026-03-27 21:43:43 -0300

    versão1
    Aula + Atividade

---
+++ atividade.py
@@ -0,0 +1,6 @@
+def calcular_media(n1, n2, n3):
+    media = (n1 + n2 + n3) / 3
+    return media
+
+resultado = calcular_media(5, 5, 7)
+print(resultado)
---
+++ aula.py
@@ -0,0 +1,13 @@
+#Criação da função
+def saudacao(nome):
+    print(f'Bem-vindo a TI, {nome}!')
+
+def somar_numeros(num1, num2):
+    return num1 + num2
+
+
+#Chamando a função
+saudacao('Igo')
+
+print('Resultado:', somar_numeros(8, 9))
+
